293d64ae6d8b82406283c3bcbb9eb90d01a27eaa
Gonzalo Pardo Villalibre <gonzalo.pardo.practicas@telefonica.com>
2020-04-27 11:43:13 +0200

    Undo: introduction to httpClient module

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +7,8 @@ import { HttpClient } from '@angular/common/http';
   ]
 })
 export class HomeComponent implements OnInit {
-  // Aquí es donde vamos a guardar la respuesta
-  paises: any[] = [];
 
-  // Al introducir el objeto http de tipo httpClient voy a poder hacer peticones http
-  constructor( private http: HttpClient) {
-    console.log('Constructor del home llamado');
-    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (datosRecibidos: any) => {
-      this.paises = datosRecibidos;
-      console.log(datosRecibidos);
-    });
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
